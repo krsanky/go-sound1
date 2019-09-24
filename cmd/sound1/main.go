aec2f3a4c5f0eb9fccb220bac6ab99a2be530f2d
paul wisehart <paul@oldcode.org>
2019-09-24 14:32:56 -0400

    asd

--- cmd/sound1/main.go
+++ cmd/sound1/main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"oldcode.org/home/wise/repo/go/sound1"
 )
 
 func main() {
 	err := sound1.Run1()
 	if err != nil {
-		fmt.Printf("err:%v\n", err)
+		panic(err)
 	}
 }
